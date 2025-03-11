6191892f9ec46840f782b9c6db352e2aa327ea7d
KFernandes001 <kalvinfernandesfk@gmail.com>
2025-03-11 10:55:19 -0300

    feat: Controle de aluno

---
+++ ApiPiaget/Data/ApiPiagetContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ApiPiaget;
+
+namespace ApiPiaget.Data
+{
+    public class ApiPiagetContext : DbContext
+    {
+        public ApiPiagetContext (DbContextOptions<ApiPiagetContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ApiPiaget.Aluno> Aluno { get; set; } = default!;
+    }
+}
